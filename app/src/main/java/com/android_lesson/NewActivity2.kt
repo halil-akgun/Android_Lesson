544a079f098fef0ff66711b16d11ae5da227bd25
Halil Akgun <halilakgun0@gmail.com>
2024-12-26 23:58:58 +0100

    data sending from one activity to another

--- app/src/main/java/com/android_lesson/NewActivity2.kt
+++ app/src/main/java/com/android_lesson/NewActivity2.kt
@@ -1,36 +1,20 @@
 package com.android_lesson
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.android_lesson.databinding.ActivityMainBinding
-import com.android_lesson.databinding.ActivityNewBinding
 
-class NewActivity : AppCompatActivity() {
-
-    private lateinit var viewBinding: ActivityNewBinding
-
+class NewActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        viewBinding = ActivityNewBinding.inflate(layoutInflater)
-        setContentView(viewBinding.root) // setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_new2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
-        // data sending from one activity to another
-        viewBinding.button11.setOnClickListener {
-            val intent = Intent(this@NewActivity, NewActivity2::class.java)
-            startActivity(intent)
-        }
-
-
     }
 }
