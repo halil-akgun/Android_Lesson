dc8c3cce8431ca1202d086454379198ee0ba1f05
Halil Akgun <halilakgun0@gmail.com>
2025-01-13 23:47:38 +0100

    NavController

--- app/src/main/java/com/android_lesson/EActivity.kt
+++ app/src/main/java/com/android_lesson/EActivity.kt
@@ -5,16 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.android_lesson.databinding.ActivityEBinding
 
 class EActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityEBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_e)
+        viewBinding = ActivityEBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Find the NavHostFragment from the fragmentContainerView
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        // Link the BottomNavigationView with the NavController for navigation
+        NavigationUI.setupWithNavController(
+            viewBinding.bottomNav,
+            navHostFragment.navController
+        )
     }
 }
