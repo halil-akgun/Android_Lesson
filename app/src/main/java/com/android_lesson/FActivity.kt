617a58b0880eddb81c22ed3aaf553603be5f14fb
Halil Akgun <halilakgun0@gmail.com>
2025-01-19 11:53:22 +0100

    title for navigation drawer

--- app/src/main/java/com/android_lesson/FActivity.kt
+++ app/src/main/java/com/android_lesson/FActivity.kt
@@ -1,9 +1,12 @@
 package com.android_lesson
 
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.addCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
@@ -19,7 +22,7 @@ class FActivity : AppCompatActivity() {
         enableEdgeToEdge()
         viewBinding = ActivityFBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activityF_DrawerLayout)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activityF)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -39,12 +42,29 @@ class FActivity : AppCompatActivity() {
 
         val toggle = ActionBarDrawerToggle(
             this,
-            viewBinding.activityFDrawerLayout,
+            viewBinding.activityF,
             viewBinding.toolbar,
             0,
             0
         )
-        viewBinding.activityFDrawerLayout.addDrawerListener(toggle)
+        viewBinding.activityF.addDrawerListener(toggle)
         toggle.syncState()
+
+        val title2 = viewBinding.sideNavActivityF.inflateHeaderView(R.layout.nav_drawer_title)
+        val title3: TextView = title2.findViewById(R.id.nav_drawer_text)
+        title3.text = "Title..."
+
+        // back button
+        onBackPressedDispatcher.addCallback(this) {
+            if (viewBinding.activityF.isDrawerOpen(GravityCompat.START)) {
+                viewBinding.activityF.closeDrawer(GravityCompat.START)
+            } else {
+                // deprecated in API 33+
+//                super.onBackPressed()
+                // Handles back button press with the modern onBackPressedDispatcher API.
+                // This method can be customized.
+                onBackPressedDispatcher.onBackPressed()
+            }
+        }
     }
 }
