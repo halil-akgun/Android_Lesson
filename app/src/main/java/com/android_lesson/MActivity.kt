1668704176f837aad08583fe4ad86b376099f1f9
Halil Akgun <halilakgun0@gmail.com>
2025-05-03 01:08:31 +0200

    Dark Mode

--- app/src/main/java/com/android_lesson/MActivity.kt
+++ app/src/main/java/com/android_lesson/MActivity.kt
@@ -3,18 +3,31 @@ package com.android_lesson
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android_lesson.databinding.ActivityMBinding
 
 class MActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_m)
+        viewBinding = ActivityMBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        viewBinding.buttonDarkModeNight.setOnClickListener {
+            delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
+        }
+        viewBinding.buttonDarkModeNotNight.setOnClickListener {
+            delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_NO
         }
     }
 }
