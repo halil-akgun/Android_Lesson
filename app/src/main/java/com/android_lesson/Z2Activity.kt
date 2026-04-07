ae9e0bf09d87099ad0bab870d97f6f2aeded67f7
Halil Akgün <halilakgun0@gmail.com>
2026-04-07 22:45:53 +0200

    Location

--- app/src/main/java/com/android_lesson/Z2Activity.kt
+++ app/src/main/java/com/android_lesson/Z2Activity.kt
@@ -2,6 +2,7 @@ package com.android_lesson
 
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -11,11 +12,16 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.android_lesson.databinding.ActivityZ2Binding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
 
 class Z2Activity : AppCompatActivity() {
 
     private lateinit var viewBinding: ActivityZ2Binding
     private var locationPermission = false
+    private lateinit var flpc: FusedLocationProviderClient
+    private lateinit var locationTask: Task<Location>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +33,8 @@ class Z2Activity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        flpc = LocationServices.getFusedLocationProviderClient(this)
 
         viewBinding.buttonGetLocation.setOnClickListener {
             locationPermission = ContextCompat.checkSelfPermission(
@@ -46,16 +54,42 @@ class Z2Activity : AppCompatActivity() {
         }
     }
 
+    fun getLocation() {
+        locationTask.addOnSuccessListener { it ->
+            if (it != null) {
+                viewBinding.textViewLocationLatitude.text =
+                    getString(R.string.latitude_format, it.latitude)
+                viewBinding.textViewLocationLongitude.text =
+                    getString(R.string.longitude_format, it.longitude)
+            } else {
+                viewBinding.textViewLocationLatitude.text =
+                    getString(R.string.location_unavailable)
+                viewBinding.textViewLocationLongitude.text =
+                    getString(R.string.location_unavailable)
+            }
+        }
+        locationTask.addOnFailureListener {
+
+        }
+    }
+
     @SuppressLint("MissingSuperCall")
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
+        locationPermission = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
         if (requestCode == 100) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 locationPermission = true
                 Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+                locationTask = flpc.lastLocation
+                getLocation()
             } else {
                 Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
             }
